2f70b7362860f4be452f6e91a3f15fc03e303cf3
icaruuuuuu <icaromachadosilva@gmail.com>
2026-05-06 22:15:18 -0300

    esqueci de comitar o resto das mudanças

--- netbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --name containernet -it --privileged \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd):/home/projeto-load-balancer \
-    containernet/containernet:latest /bin/bash
